cede62655b4d691bbfc312d6ec5217e769196bf5
Menno-Paul Schreuder <menno@pixelsforbreakfast.nl>
2020-09-24 20:59:19 +0200

    Split into separate properties to prevent x-axis from being dominant

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const plugin = require('tailwindcss/plugin');
 module.exports = plugin(function ({ variants, addUtilities }) {
     addUtilities(
         {
-            '.make-fullwidth': { margin: '0 calc(50% - 50vw)' }
+            '.make-fullwidth': {
+                'margin-left': 'calc(50% - 50vw)',
+                'margin-right': 'calc(50% - 50vw)',
+              }
         },
         variants('makeFullwidth')
     )
